ed8fb2a1b9d5a28626d5efe9eae605f4f5abcba3
Sayan Banerjee <sayanintouch25@gmail.com>
2025-12-27 16:26:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
-def main():
-    return "Welcome!"
-
-@app.route('/how are you')
 def hello():
-    return 'I am good, how about you?'
+    return "Hello, Flask is running in a container!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=5000)
